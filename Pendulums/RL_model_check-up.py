bc34854369f08f4ad3a230d1f2517c45018f0022
Oleg Rogov <Oleg.Rogov@lut.fi>
2023-09-21 15:01:18 +0300

    cleaning up

---
+++ Pendulums/RL_model_check-up.py
@@ -0,0 +1,51 @@
+import os
+import moviepy.video.io.ImageSequenceClip
+import matplotlib.pyplot as plt
+
+import exudyn as exu
+from exudyn.utilities import *
+from exudynNLinkLibV2continuous import InvertedNPendulumEnv
+from stable_baselines3 import A2C, PPO, SAC, TD3, DDPG
+from exudyn.interactive import SolutionViewer
+
+# env initialization
+localEnv = InvertedNPendulumEnv(nArms = 3,
+                                massArmFact = 1, massCartFact = 1, lengthFact = 1,
+                                relativeFriction = 0,
+                                stepUpdateTime = 0.02,
+                                cartForce = 60,
+                                actionThreshold = 1,
+                                forceFactor = 1,
+                                rewardMode = 2)
+
+localEnv.randomInitializationValue = 0.1
+
+# visualize the untrained multibody model
+solFileName = 'solution.txt'
+
+localEnv.TestModel(numberOfSteps = int(10/0.02), timesteps = 0.02, model = None,
+                  useRenderer = True, sleepTime = 0, showTimeSpent = False,
+                  saveAnimation = False, solutionFileName = solFileName)
+
+sol = LoadSolutionFile('solution.txt')
+SolutionViewer(localEnv.mbs, sol)
+
+# load the trained model
+model = PPO.load('models\TP_PPO_r2_n64_v2_Model0_PPO_186368.zip')
+
+# test the trained model and visualize it
+localEnv.TestModel(numberOfSteps = int(10/0.02), timesteps = 0.02, model = model,
+                  useRenderer = True, sleepTime = 0, showTimeSpent = False,
+                  saveAnimation = False, solutionFileName = solFileName)
+
+sol = LoadSolutionFile('solution.txt')
+SolutionViewer(localEnv.mbs, sol)
+
+image_folder='images'
+fps=30
+
+image_files = [os.path.join(image_folder, img)
+               for img in os.listdir(image_folder)
+               if img.endswith(".png")]
+clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+clip.write_videofile('TP_PPO_186k_79tests_r2.mp4')
---
+++ Pendulums/RewardPlots.py
@@ -0,0 +1,63 @@
+import re
+import matplotlib.pyplot as plt
+
+file_path_0 = "models\TP_PPO_r2_n64_v2_Results0.txt"
+file_path_1 = "models\TP_PPO_r2_n64_v2_Results1.txt"
+file_path_2 = "models\TP_PPO_r2_n64_v2_Results2.txt"
+
+total_steps_0 = []
+reward_per_episode_0 = []
+total_steps_1 = []
+reward_per_episode_1 = []
+total_steps_2 = []
+reward_per_episode_2 = []
+
+with open(file_path_0, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        total_steps_match = re.search(r"'totalSteps': (\d+),", line)
+        reward_match = re.search(r"'rewardPerEpisode': ([\d.]+)", line)
+
+        if total_steps_match:
+            total_steps_0.append(int(total_steps_match.group(1)))
+        
+        if reward_match:
+            reward_per_episode_0.append(float(reward_match.group(1)))
+            
+with open(file_path_1, 'r') as file:
+    lines = file.readlines()[6:-1]
+    for line in lines:
+        total_steps_match = re.search(r"'totalSteps': (\d+),", line)
+        reward_match = re.search(r"'rewardPerEpisode': ([\d.]+)", line)
+
+        if total_steps_match:
+            total_steps_1.append(int(total_steps_match.group(1)))
+        
+        if reward_match:
+            reward_per_episode_1.append(float(reward_match.group(1)))
+            
+with open(file_path_2, 'r') as file:
+    lines = file.readlines()[6:-1]
+    for line in lines:
+        total_steps_match = re.search(r"'totalSteps': (\d+),", line)
+        reward_match = re.search(r"'rewardPerEpisode': ([\d.]+)", line)
+
+        if total_steps_match:
+            total_steps_2.append(int(total_steps_match.group(1)))
+        
+        if reward_match:
+            reward_per_episode_2.append(float(reward_match.group(1)))    
+   
+plt.figure(figsize=(10,6))
+ax = plt.subplot(1, 1, 1)
+
+# Plot the data
+plt.plot(total_steps_0, reward_per_episode_0, label='case 1')
+plt.plot(total_steps_1, reward_per_episode_1, label='case 2')
+plt.plot(total_steps_2, reward_per_episode_2, label='case 3')
+
+# Add labels and title
+plt.xlabel('Total Training Steps')
+plt.ylabel('Reward per Episode')
+plt.legend()
+plt.show()
